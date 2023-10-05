e0c596b403ad9fac7be0d472869d8572d8135a50
mhmtcevik <mhmtcevik@gmail.com>
2023-10-05 19:50:06 +0300

    Folders have been created.

---
+++ PokemonApp/Scenes/Favorite/ViewModel/FavoriteViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteViewModel.swift
+//  PokemonApp
+//
+//  Created by Mehmet Çevık on 5.10.2023.
+//
+
+import Foundation
---
+++ PokemonApp/Scenes/Search/ViewModel/SearchViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchViewModel.swift
+//  PokemonApp
+//
+//  Created by Mehmet Çevık on 5.10.2023.
+//
+
+import Foundation
