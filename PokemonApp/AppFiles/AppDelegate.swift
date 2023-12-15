f83643dcfabee58e054e3b44d94bdbdb544dd817
mhmtcevik <mhmtcevik@gmail.com>
2023-12-15 15:04:05 +0300

    AppDelegate print commit

--- PokemonApp/AppFiles/AppDelegate.swift
+++ PokemonApp/AppFiles/AppDelegate.swift
@@ -12,6 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        print("App is being started..")
         return true
     }
 
